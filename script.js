4a0881496e2cdbae5b87effeb37640b7a31963e3
Siva Prasath <sivaprasathchellamuthu@gmail.com>
2024-10-05 02:26:02 +0530

    added functions for snakes and ladder and added some audios for snakes and ladders

--- script.js
+++ script.js
@@ -25,31 +25,140 @@ function grids() {
 }
 grids();
 
-//==================================================================================================================================
+//=========================================================================================================================
 
-coin=document.querySelectorAll(".box")
+coin=document.querySelectorAll(".box") 
 var start=1
 currentstep=0
 function rolldice(){ 
+          document.getElementById("rolldice").disabled = true;
+          setTimeout(function() {
+          document.getElementById("rolldice").disabled = false;
+         }, 2000);
+        var snakeahh= new Audio("asset/snake_ahh.wav");
+        var ladderhur=new Audio("asset/ladderhur.wav");
         var rollmp3 = new Audio("asset/dice-roll-sound.mp3");
+        var winsound=new Audio("asset/winsound.wav");
         rollmp3.play();
         x=1;
         animate=setInterval(()=>{
           newstep=Math.floor(Math.random() * 6) + 1;
-          document.getElementById("dice-res").innerHTML=newstep
+          document.getElementById("dice-res").innerHTML=newstep 
         if(x>10)
           {
            clearInterval(animate);
+           if(currentstep==95 && (newstep==6)){
+            newstep=0
+            document.getElementById("dice-res").innerHTML="No Move"
+        }
+        if(currentstep==96 && (newstep==5||newstep==6)){
+          newstep=0
+          document.getElementById("dice-res").innerHTML="No Move"
+      }
+      if(currentstep==97 && (newstep==4||newstep==5||newstep==6)){
+        newstep=0
+        document.getElementById("dice-res").innerHTML="No Move"
+    }
+    if(currentstep==98 &&(newstep==3||newstep==4||newstep==5||newstep==6)){
+      newstep=0
+      document.getElementById("dice-res").innerHTML="No Move"
+  }
+  if(currentstep==99 &&(newstep==2||newstep==3||newstep==4||newstep==5||newstep==6)){
+    newstep=0
+    document.getElementById("dice-res").innerHTML="No Move"
+}
+// console.log((currentstep==98) && (newstep==3||newstep==4||newstep==5||newstep==6))
+           beforestate=currentstep
            currentstep=currentstep+newstep
+          //  console.log(currentstep)
+           if(currentstep==100){
+          
+                  document.getElementById("dice-res").innerHTML="Won"
+                  winsound.play();
+                  setTimeout(function() { location.reload(1); }, 5000);
+           }
+          else if(currentstep==17 ){  //snake or ladder box actual
+                   
+                  coin[35].innerHTML= '<img src="asset/squid circle.png" alt="squid">'  //resultbox index(box -1)
+                  coin[beforestate-1].innerHTML=`${beforestate} `  //replacd old box index with prev number(actual box number)
+                  currentstep=36   //setting new pos
+                  ladderhur.play();
+           }
+
+           else if(currentstep==33 ){  
+        
+            coin[14].innerHTML= '<img src="asset/squid circle.png" alt="squid">'  
+            coin[beforestate-1].innerHTML=`${beforestate} ` 
+            currentstep=15 
+            snakeahh.play();
+           }
+
+           else if(currentstep==48 ){  
+           
+                  coin[69].innerHTML= '<img src="asset/squid circle.png" alt="squid">' 
+                  coin[beforestate-1].innerHTML=`${beforestate} ` 
+                  currentstep=70  
+                  ladderhur.play();
+             }
+             else if(currentstep==79){  
+           
+                 coin[43].innerHTML= '<img src="asset/squid circle.png" alt="squid">' 
+                 coin[beforestate-1].innerHTML=`${beforestate} ` 
+                 currentstep=44
+                 snakeahh.play();
+              }
+
+              else if(currentstep==55 ){
+            
+                  coin[26].innerHTML= '<img src="asset/squid circle.png" alt="squid">' 
+                  coin[beforestate-1].innerHTML=`${beforestate} ` 
+                  currentstep=27
+                  snakeahh.play();
+              }
+
+              else if(currentstep==56){  
+            
+                  coin[76].innerHTML= '<img src="asset/squid circle.png" alt="squid">' 
+                  coin[beforestate-1].innerHTML=`${beforestate} ` 
+                  currentstep=77 
+                  ladderhur.play();
+               }
+
+               else if(currentstep==62 ){   
+        
+                  coin[19].innerHTML= '<img src="asset/squid circle.png" alt="squid">' 
+                  coin[beforestate-1].innerHTML=`${beforestate} ` 
+                  currentstep=20   
+                  snakeahh.play();
+                }
+
+                else if(currentstep==73 ){  
+            
+                   coin[93].innerHTML= '<img src="asset/squid circle.png" alt="squid">'  
+                   coin[beforestate-1].innerHTML=`${beforestate} `  
+                   currentstep=94  
+                   ladderhur.play();
+              }
+
+              else if(currentstep==96 ){ 
+        
+                  coin[75].innerHTML= '<img src="asset/squid circle.png" alt="squid">'   
+                  coin[beforestate-1].innerHTML=`${beforestate} ` 
+                  currentstep=76  
+                  snakeahh.play();
+                    }
+           else{
             move(currentstep)
+           }
+           
           }
-           x++; I
+           x++;   
         },100);
         // newstep=Math.floor(Math.random() * 6) + 1;
         // document.getElementById("dice-res").innerHTML=newstep     
 }
 
-
+ 
 function move(step){
     stop=step
     function play()
@@ -63,10 +172,11 @@ function move(step){
     start++;
     }
     }
-    setInterval(play,1000)
+    setInterval(play,200)
 }
 // coin[1].innerHTML= '<img src="asset/squid circle.png" alt="smurf">'
 //============================================================================================================================================
+
 
 function myaudio(){
     const state=document.getElementById("audiobg").innerText;
